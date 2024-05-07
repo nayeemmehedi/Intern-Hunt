65fdd1bb06dcd92ee4e399424700c0a8986e512f
nayeemmehedi <boyneel79@gmail.com>
2024-05-08 04:08:13 +0600

    some link issue

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-// import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import {
   QueryClient,
